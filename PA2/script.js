9c9d43ed6572d47d604d65dc5f40803db3d5ef2a
Faiz Surani <faiz.surani@gmail.com>
2021-01-26 23:27:18 -0800

    Add scroll button

--- PA2/script.js
+++ PA2/script.js
@@ -7,3 +7,25 @@ function validateEmail() {
         statusDiv.innerHTML = "Invalid email address.";
     }
 }
+
+document.onscroll = () => {
+    const scrollFraction = window.pageYOffset / window.innerHeight;
+    const existingElement = document.getElementById("scroll-button");
+    if (scrollFraction > 0.25) {
+        if (existingElement) {
+            existingElement.style.opacity = 1;
+            return;
+        }
+        const element = document.createElement("button")
+        element.classList.add("scroll-button");
+        element.id = "scroll-button"
+        element.innerHTML = "Scroll to top"
+        element.onclick = () => {
+            window.scrollTo(0, 0);
+        }
+        document.body.appendChild(element);
+        console.log("scrolly scroll");
+    } else {
+        existingElement.style.opacity = "0";
+    }
+}
