b1239951838f659336f7097a4c7cfb4f191acb18
Faiz Surani <faiz.surani@gmail.com>
2021-01-27 17:19:57 -0800

    Add media modals

--- PA2/script.js
+++ PA2/script.js
@@ -13,19 +13,48 @@ document.onscroll = () => {
     const existingElement = document.getElementById("scroll-button");
     if (scrollFraction > 0.25) {
         if (existingElement) {
-            existingElement.style.opacity = 1;
+            existingElement.style.opacity = "1";
             return;
         }
-        const element = document.createElement("button")
-        element.classList.add("scroll-button");
+        const element = document.createElement("button");
         element.id = "scroll-button"
         element.innerHTML = "Scroll to top"
         element.onclick = () => {
             window.scrollTo(0, 0);
         }
         document.body.appendChild(element);
-        console.log("scrolly scroll");
-    } else {
+    } else if (existingElement) {
         existingElement.style.opacity = "0";
     }
 }
+
+document.querySelectorAll(".photo-item").forEach((item) => {
+    item.addEventListener("click", (event) => {
+        event.stopPropagation();
+        const modal = document.getElementById("media-modal");
+        const modalContent = document.getElementById("modal-content");
+        const mediaNode = event.target.cloneNode(true);
+        console.log(mediaNode.tagName);
+        if (mediaNode.tagName === "VIDEO") {
+            mediaNode.setAttribute("controls", true);
+        }
+        mediaNode.style.margin = "0 auto";
+        modalContent.append(mediaNode);
+        modal.style.display = "block";
+    });
+});
+
+document.querySelectorAll("#media-modal").forEach((item) => {
+    item.addEventListener("click", (event) => {
+        const modal = document.getElementById("media-modal");
+        const modalContent = document.getElementById("modal-content");
+        modal.style.display = "none";
+        modalContent.innerHTML = "";
+    });
+});
+
+document.querySelectorAll("#modal-content").forEach((item) => {
+    item.addEventListener("click", (event) => {
+        event.stopPropagation();
+    });
+});
