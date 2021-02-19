7b0f607df4ae5d10d0fe2f98b1e76843556e55f3
Faiz Surani <faiz.surani@gmail.com>
2021-02-18 17:15:55 -0800

    Add page map in App

--- PA3/src/App.jsx
+++ PA3/src/App.jsx
@@ -2,11 +2,39 @@ import './App.css';
 import {Component} from "react";
 import TabList from "./TabList";
 import Body from "./Body";
+import Text from "./Text";
+import Images from "./Images";
+import Videos from "./Videos";
+import Table from "./Table";
+import Email from "./Email";
+
+export const PageMap = {
+    text: {
+        name: "Text",
+        component: Text,
+    },
+    images: {
+        name: "Images",
+        component: Images,
+    },
+    videos: {
+        name: "Videos",
+        component: Videos
+    },
+    table: {
+        name: "Table",
+        component: Table,
+    },
+    email: {
+        name: "Email",
+        component: Email,
+    },
+}
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {activeTabId: ""};
+        this.state = {activeTabId: "text"};
     }
 
     setActiveTab = (activeTabId) => {
